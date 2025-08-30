f4b689adf723473f3d99b09919bb14b93c2a9a07
Ishaan Dandekar <81032792+ishaan-dandekar@users.noreply.github.com>
2025-08-30 10:18:11 +0530

    Update extension.js

--- extension.js
+++ extension.js
@@ -1,16 +1,17 @@
-const { GLib, Gio } = imports.gi;
-const EndSessionDialog = imports.ui.endSessionDialog;
-const ExtensionUtils = imports.misc.extensionUtils;
+import GLib from 'gi://GLib';
+import Gio from 'gi://Gio';
+import * as EndSessionDialog from 'resource:///org/gnome/shell/ui/endSessionDialog.js';
+import {Extension, gettext as _} from 'resource:///org/gnome/shell/extensions/extension.js';
 
-class Extension {
-    constructor() {
+export default class RestartIntoExtension extends Extension {
+    constructor(metadata) {
+        super(metadata);
         this.originalUpdateButtons = null;
         this.settings = null;
     }
     
     enable() {
-        // Overrides GNOME's EndSessionDialog to add a custom restart button when the restart dialog appears (this._type === 2)
-        this.settings = ExtensionUtils.getSettings('org.gnome.shell.extensions.restartinto');
+        this.settings = this.getSettings('org.gnome.shell.extensions.restartinto');
         this.originalUpdateButtons = EndSessionDialog.EndSessionDialog.prototype._updateButtons;
         let extension = this;
         
@@ -22,34 +23,73 @@ class Extension {
                 const buttonText = extension.settings.get_string('button-text');
                 const debugMode = extension.settings.get_boolean('debug-mode');
                 
-                this.addButton({
-                    action: () => {
-                        this.close();
-                        extension._restartInto(bootId, debugMode);
-                    },
-                    label: buttonText,
-                    key: 1
-                });
+                // Check if pkexec wrapper script exists and is executable
+                if (extension._checkPkexecScript()) {
+                    this.addButton({
+                        action: () => {
+                            this.close();
+                            extension._restartInto(bootId, debugMode);
+                        },
+                        label: buttonText,
+                        key: 1
+                    });
+                } else if (debugMode) {
+                    console.log('RestartInto: pkexec wrapper script not found or not executable');
+                }
             }
         };
     }
     
+    _checkPkexecScript() {
+        // Check if the pkexec wrapper script exists and is executable
+        const scriptPath = GLib.get_home_dir() + '/.local/bin/restartinto-helper';
+        const file = Gio.File.new_for_path(scriptPath);
+        
+        try {
+            const fileInfo = file.query_info('standard::type,access::can-execute', 
+                                            Gio.FileQueryInfoFlags.NONE, null);
+            return fileInfo.get_file_type() === Gio.FileType.REGULAR && 
+                   fileInfo.get_attribute_boolean('access::can-execute');
+        } catch (e) {
+            return false;
+        }
+    }
+    
     _restartInto(bootId, debugMode) {
         if (debugMode) {
-            console.log(`Restarting to OS with boot ID: ${bootId}`);
+            console.log(`RestartInto: Attempting to restart to OS with boot ID: ${bootId}`);
         }
-        // EFI Boot Management: Uses efibootmgr to set the next boot entry and immediately reboots using the command
-        const command = `bash -c "sudo efibootmgr -n ${bootId} && sudo reboot"`;
+        
+        // Use pkexec to execute a helper script with elevated privileges
+        const scriptPath = GLib.get_home_dir() + '/.local/bin/restartinto-helper';
+        const command = ['pkexec', scriptPath, bootId];
         
         if (debugMode) {
-            console.log(`Executing command: ${command}`);
+            console.log(`RestartInto: Executing command: ${command.join(' ')}`);
         }
         
         try {
-            GLib.spawn_command_line_async(command);
+            const proc = Gio.Subprocess.new(
+                command,
+                Gio.SubprocessFlags.STDOUT_PIPE | Gio.SubprocessFlags.STDERR_PIPE
+            );
+            
+            proc.communicate_utf8_async(null, null, (proc, res) => {
+                try {
+                    const [, stdout, stderr] = proc.communicate_utf8_finish(res);
+                    if (debugMode) {
+                        if (stdout) console.log(`RestartInto stdout: ${stdout}`);
+                        if (stderr) console.log(`RestartInto stderr: ${stderr}`);
+                    }
+                } catch (e) {
+                    if (debugMode) {
+                        console.error('RestartInto: Failed to execute restart command:', e);
+                    }
+                }
+            });
         } catch (e) {
             if (debugMode) {
-                console.error('Failed to execute restart command:', e);
+                console.error('RestartInto: Failed to create subprocess:', e);
             }
         }
     }
@@ -61,8 +101,4 @@ class Extension {
         }
         this.settings = null;
     }
-}
-
-function init() {
-    return new Extension();
 }
