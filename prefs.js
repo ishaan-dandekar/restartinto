38ca97f3dabde732d170073c51a9fd6e32ff6ed2
Ishaan Dandekar <81032792+ishaan-dandekar@users.noreply.github.com>
2025-08-30 10:18:32 +0530

    Update prefs.js

--- prefs.js
+++ prefs.js
@@ -1,186 +1,224 @@
-const { Gio, Gtk, GLib } = imports.gi;
-const ExtensionUtils = imports.misc.extensionUtils;
+import Gio from 'gi://Gio';
+import Adw from 'gi://Adw';
+import Gtk from 'gi://Gtk';
+import GLib from 'gi://GLib';
 
-function init() {
-}
+import {ExtensionPreferences, gettext as _} from 'resource:///org/gnome/Shell/Extensions/js/extensions/prefs.js';
 
-function buildPrefsWidget() {
-    const settings = ExtensionUtils.getSettings('org.gnome.shell.extensions.restartinto');
-    
-    // Create main container
-    const widget = new Gtk.Box({
-        orientation: Gtk.Orientation.VERTICAL,
-        spacing: 20,
-        margin_top: 20,
-        margin_bottom: 20,
-        margin_start: 20,
-        margin_end: 20,
-    });
+export default class RestartIntoPreferences extends ExtensionPreferences {
+    fillPreferencesWindow(window) {
+        const settings = this.getSettings('org.gnome.shell.extensions.restartinto');
 
-    // Boot ID Section
-    const bootFrame = new Gtk.Frame({
-        label: 'Boot Configuration',
-        margin_bottom: 20,
-    });
-    
-    const bootBox = new Gtk.Box({
-        orientation: Gtk.Orientation.VERTICAL,
-        spacing: 10,
-        margin_top: 10,
-        margin_bottom: 10,
-        margin_start: 10,
-        margin_end: 10,
-    });
-    
-    const bootLabel = new Gtk.Label({
-        label: 'Windows Boot Entry ID (4 hex digits, e.g. 0000):',
-        halign: Gtk.Align.START,
-    });
-    
-    const bootEntry = new Gtk.Entry({
-        text: settings.get_string('boot-id'),
-        max_length: 4,
-    });
-    
-    bootEntry.connect('changed', () => {
-        const text = bootEntry.get_text();
-        if (text.length === 4 && /^[0-9A-Fa-f]{4}$/.test(text)) {
-            settings.set_string('boot-id', text);
-        }
-    });
-    
-    bootBox.append(bootLabel);
-    bootBox.append(bootEntry);
-    bootFrame.set_child(bootBox);
-    widget.append(bootFrame);
+        // Create preferences page
+        const page = new Adw.PreferencesPage({
+            title: _('General'),
+            icon_name: 'dialog-information-symbolic',
+        });
+        window.add(page);
 
-    // Button Text Section
-    const buttonFrame = new Gtk.Frame({
-        label: 'Button Settings',
-        margin_bottom: 20,
-    });
-    
-    const buttonBox = new Gtk.Box({
-        orientation: Gtk.Orientation.VERTICAL,
-        spacing: 10,
-        margin_top: 10,
-        margin_bottom: 10,
-        margin_start: 10,
-        margin_end: 10,
-    });
-    
-    const buttonTextLabel = new Gtk.Label({
-        label: 'Button Text:',
-        halign: Gtk.Align.START,
-    });
-    
-    const buttonTextEntry = new Gtk.Entry({
-        text: settings.get_string('button-text'),
-    });
-    
-    buttonTextEntry.connect('changed', () => {
-        settings.set_string('button-text', buttonTextEntry.get_text());
-    });
-    
-    buttonBox.append(buttonTextLabel);
-    buttonBox.append(buttonTextEntry);
-    buttonFrame.set_child(buttonBox);
-    widget.append(buttonFrame);
+        // Boot Configuration Group
+        const bootGroup = new Adw.PreferencesGroup({
+            title: _('Boot Configuration'),
+            description: _('Configure the target OS boot entry'),
+        });
+        page.add(bootGroup);
 
-    // Options Section
-    const optionsFrame = new Gtk.Frame({
-        label: 'Options',
-        margin_bottom: 20,
-    });
-    
-    const optionsBox = new Gtk.Box({
-        orientation: Gtk.Orientation.VERTICAL,
-        spacing: 10,
-        margin_top: 10,
-        margin_bottom: 10,
-        margin_start: 10,
-        margin_end: 10,
-    });
-    
-    // Debug mode checkbox
-    const debugCheck = new Gtk.CheckButton({
-        label: 'Enable debug logging',
-        active: settings.get_boolean('debug-mode'),
-    });
-    
-    debugCheck.connect('toggled', () => {
-        settings.set_boolean('debug-mode', debugCheck.get_active());
-    });
-    
-    optionsBox.append(debugCheck);
-    optionsFrame.set_child(optionsBox);
-    widget.append(optionsFrame);
-
-    // Boot entries detection section
-    const detectionFrame = new Gtk.Frame({
-        label: 'Boot Entry Detection',
-    });
-    
-    const detectionBox = new Gtk.Box({
-        orientation: Gtk.Orientation.VERTICAL,
-        spacing: 10,
-        margin_top: 10,
-        margin_bottom: 10,
-        margin_start: 10,
-        margin_end: 10,
-    });
-    
-    const detectionLabel = new Gtk.Label({
-        label: 'Click to scan for all available boot entries:',
-        halign: Gtk.Align.START,
-    });
-    
-    const scanButton = new Gtk.Button({
-        label: 'Scan All Boot Entries',
-    });
-    
-    const resultLabel = new Gtk.Label({
-        label: '',
-        halign: Gtk.Align.START,
-        wrap: true,
-        selectable: true,
-    });
-    
-    scanButton.connect('clicked', () => {
-        try {
-            const [success, stdout, stderr] = GLib.spawn_command_line_sync('efibootmgr');
-            if (success) {
-                const lines = new TextDecoder().decode(stdout).split('\n');
-                const bootEntries = [];
-                
-                lines.forEach(line => {
-                    const match = line.match(/Boot([0-9A-Fa-f]{4})\*?\s+(.+)/);
-                    if (match) {
-                        const bootId = match[1];
-                        const bootName = match[2];
-                        bootEntries.push(`${bootId}: ${bootName}`);
-                    }
-                });
-                
-                if (bootEntries.length > 0) {
-                    resultLabel.set_text('Found boot entries:\n' + bootEntries.join('\n'));
-                } else {
-                    resultLabel.set_text('No boot entries found');
-                }
-            } else {
-                resultLabel.set_text('Error: Could not run efibootmgr');
+        // Boot ID Row
+        const bootRow = new Adw.EntryRow({
+            title: _('Boot Entry ID'),
+            text: settings.get_string('boot-id'),
+            input_purpose: Gtk.InputPurpose.DIGITS,
+        });
+        bootRow.set_input_hints(Gtk.InputHints.UPPERCASE_CHARS);
+        bootRow.connect('changed', () => {
+            const text = bootRow.get_text();
+            if (text.length <= 4 && /^[0-9A-Fa-f]*$/.test(text)) {
+                settings.set_string('boot-id', text.toUpperCase());
             }
-        } catch (e) {
-            resultLabel.set_text('Error: ' + e.message);
-        }
-    });
-    
-    detectionBox.append(detectionLabel);
-    detectionBox.append(scanButton);
-    detectionBox.append(resultLabel);
-    detectionFrame.set_child(detectionBox);
-    widget.append(detectionFrame);
+        });
+        bootGroup.add(bootRow);
 
-    widget.show_all ? widget.show_all() : widget.show();
-    return widget;
+        // Button Settings Group
+        const buttonGroup = new Adw.PreferencesGroup({
+            title: _('Button Settings'),
+            description: _('Customize the restart button appearance'),
+        });
+        page.add(buttonGroup);
+
+        // Button Text Row
+        const buttonTextRow = new Adw.EntryRow({
+            title: _('Button Text'),
+            text: settings.get_string('button-text'),
+        });
+        buttonTextRow.connect('changed', () => {
+            settings.set_string('button-text', buttonTextRow.get_text());
+        });
+        buttonGroup.add(buttonTextRow);
+
+        // Options Group
+        const optionsGroup = new Adw.PreferencesGroup({
+            title: _('Options'),
+        });
+        page.add(optionsGroup);
+
+        // Debug Mode Row
+        const debugRow = new Adw.SwitchRow({
+            title: _('Debug Mode'),
+            subtitle: _('Enable debug logging to system journal'),
+            active: settings.get_boolean('debug-mode'),
+        });
+        debugRow.connect('notify::active', () => {
+            settings.set_boolean('debug-mode', debugRow.active);
+        });
+        optionsGroup.add(debugRow);
+
+        // Boot Entry Detection Group
+        const detectionGroup = new Adw.PreferencesGroup({
+            title: _('Boot Entry Detection'),
+            description: _('Find available EFI boot entries'),
+        });
+        page.add(detectionGroup);
+
+        // Scan Button Row
+        const scanRow = new Adw.ActionRow({
+            title: _('Scan Boot Entries'),
+            subtitle: _('Click to detect all available boot entries'),
+        });
+
+        const scanButton = new Gtk.Button({
+            label: _('Scan'),
+            valign: Gtk.Align.CENTER,
+            css_classes: ['suggested-action'],
+        });
+
+        scanButton.connect('clicked', () => {
+            this._scanBootEntries(window, settings);
+        });
+
+        scanRow.add_suffix(scanButton);
+        detectionGroup.add(scanRow);
+
+        // Setup Instructions Group
+        const setupGroup = new Adw.PreferencesGroup({
+            title: _('Setup Instructions'),
+            description: _('Required setup for the extension to work'),
+        });
+        page.add(setupGroup);
+
+        const setupRow = new Adw.ActionRow({
+            title: _('Helper Script Required'),
+            subtitle: _('This extension requires a helper script with pkexec privileges'),
+        });
+
+        const helpButton = new Gtk.Button({
+            label: _('Show Instructions'),
+            valign: Gtk.Align.CENTER,
+            css_classes: ['accent'],
+        });
+
+        helpButton.connect('clicked', () => {
+            this._showSetupInstructions(window);
+        });
+
+        setupRow.add_suffix(helpButton);
+        setupGroup.add(setupRow);
+    }
+
+    _scanBootEntries(window, settings) {
+        try {
+            const proc = Gio.Subprocess.new(
+                ['efibootmgr'],
+                Gio.SubprocessFlags.STDOUT_PIPE | Gio.SubprocessFlags.STDERR_PIPE
+            );
+
+            proc.communicate_utf8_async(null, null, (proc, res) => {
+                try {
+                    const [, stdout, stderr] = proc.communicate_utf8_finish(res);
+                    
+                    if (stdout) {
+                        const lines = stdout.split('\n');
+                        const bootEntries = [];
+                        
+                        lines.forEach(line => {
+                            const match = line.match(/Boot([0-9A-Fa-f]{4})\*?\s+(.+)/);
+                            if (match) {
+                                const bootId = match[1];
+                                const bootName = match[2];
+                                bootEntries.push(`${bootId}: ${bootName}`);
+                            }
+                        });
+
+                        this._showBootEntriesDialog(window, bootEntries);
+                    } else {
+                        this._showErrorDialog(window, _('No output from efibootmgr'));
+                    }
+                } catch (e) {
+                    this._showErrorDialog(window, e.message);
+                }
+            });
+        } catch (e) {
+            this._showErrorDialog(window, _('Could not run efibootmgr: ') + e.message);
+        }
+    }
+
+    _showBootEntriesDialog(window, bootEntries) {
+        const dialog = new Adw.AlertDialog({
+            heading: _('Available Boot Entries'),
+            body: bootEntries.length > 0 ? bootEntries.join('\n') : _('No boot entries found'),
+        });
+
+        dialog.add_response('close', _('Close'));
+        dialog.present(window);
+    }
+
+    _showErrorDialog(window, message) {
+        const dialog = new Adw.AlertDialog({
+            heading: _('Error'),
+            body: message,
+        });
+
+        dialog.add_response('close', _('Close'));
+        dialog.present(window);
+    }
+
+    _showSetupInstructions(window) {
+        const instructions = `To use this extension, you need to create a helper script:
+
+1. Create the directory:
+   mkdir -p ~/.local/bin
+
+2. Create the script ~/.local/bin/restartinto-helper:
+   #!/bin/bash
+   if [ "$#" -ne 1 ]; then
+       echo "Usage: $0 <boot_id>"
+       exit 1
+   fi
+   
+   BOOT_ID="$1"
+   
+   if [[ ! "$BOOT_ID" =~ ^[0-9A-Fa-f]{4}$ ]]; then
+       echo "Error: Invalid boot ID format"
+       exit 1
+   fi
+   
+   efibootmgr -n "$BOOT_ID" && reboot
+
+3. Make it executable:
+   chmod +x ~/.local/bin/restartinto-helper
+
+4. Create a pkexec policy file at:
+   /usr/share/polkit-1/actions/org.gnome.shell.extensions.restartinto.policy
+
+   (You'll need to create this manually with appropriate PolicyKit configuration)
+
+Note: This extension requires administrative privileges to modify boot entries.`;
+
+        const dialog = new Adw.AlertDialog({
+            heading: _('Setup Instructions'),
+            body: instructions,
+        });
+
+        dialog.add_response('close', _('Close'));
+        dialog.present(window);
+    }
 }
